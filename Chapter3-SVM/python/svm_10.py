84ab225c8d41e80f1c59126c525715e40c8cba6f
HatcherRobotics <2415983580@qq.com>
2023-03-13 20:10:41 +0800

    finish Chapter3

--- Chapter3-SVM/python/svm_10.py
+++ Chapter3-SVM/python/svm_10.py
@@ -5,7 +5,7 @@ from sklearn import metrics
 from sklearn import model_selection
 import numpy as np
 
-df = pd.read_excel('CTG.xls','Data')
+df = pd.read_excel('/home/hatcher/test/intelligent-information-processing/Chapter3-SVM/python/CTG.xls','Data')
 df.fillna(0,inplace=True)
 
 features = np.array(df.loc[1:2126,1:21])
@@ -13,7 +13,7 @@ target = np.array(df.loc[1:2126,22]).astype('int')
 
 x_train,x_test,y_train,y_test = model_selection.train_test_split(features,target,random_state=1,test_size=0.2)
 
-classifier = svm.SVC(kernel='linear',gamma=0.1,decision_function_shape='ovo',C=0.1)
+classifier = svm.SVC(kernel='poly',gamma='auto',degree=2,decision_function_shape='ovo',C=1)
 classifier.fit(x_train,y_train)
 print("SVM-输出训练集的准确率为：",classifier.score(x_train,y_train))
 print("SVM-输出测试集的准确率为：",classifier.score(x_test,y_test))
